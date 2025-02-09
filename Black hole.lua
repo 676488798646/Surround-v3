063bf513073f080fba5646d2d74a3641b5904d14
676488798646 <2163611869@qq.com>
2025-02-09 18:48:02 +0800

    Update Black hole.lua

--- Black hole.lua
+++ Black hole.lua
@@ -1,4 +1,4 @@
-555local Players = game:GetService("Players")
+local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local SoundService = game:GetService("SoundService")
